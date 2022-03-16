aae30b96da74a49d538402720e9638f234ae074a
Jonathan Bell <jon@jonbell.net>
2022-03-16 13:13:39 -0400

    Switch CI to use testWithSplitGenerator

--- scripts/experiments/generate-experiment-matrix.py
+++ scripts/experiments/generate-experiment-matrix.py
@@ -2,15 +2,15 @@ import sys
 import json
 
 bms = [
-{"id":"ant", "class":"edu.berkeley.cs.jqf.examples.ant.ProjectBuilderTest", "method" : "testWithGenerator",
+{"id":"ant", "class":"edu.berkeley.cs.jqf.examples.ant.ProjectBuilderTest", "method" : "testWithSplitGenerator",
 "coveragePackages" : "org/apache/tools/ant/*"},
-{"id":"bcel", "class":"edu.berkeley.cs.jqf.examples.bcel.ParserTest", "method" : "testWithGenerator",
+{"id":"bcel", "class":"edu.berkeley.cs.jqf.examples.bcel.ParserTest", "method" : "testWithSplitGenerator",
 "coveragePackages" : "org/apache/bcel/*"},
-{"id":"closure", "class" : "edu.berkeley.cs.jqf.examples.closure.CompilerTest", "method": "testWithGenerator",
+{"id":"closure", "class" : "edu.berkeley.cs.jqf.examples.closure.CompilerTest", "method": "testWithSplitGenerator",
 "coveragePackages" : "com/google/javascript/jscomp/*"},
-{"id":"maven", "class" : "edu.berkeley.cs.jqf.examples.maven.ModelReaderTest", "method" : "testWithGenerator",
+{"id":"maven", "class" : "edu.berkeley.cs.jqf.examples.maven.ModelReaderTest", "method" : "testWithSplitGenerator",
 "coveragePackages" : "org/apache/maven/model/*"},
-{"id":"rhino", "class" : "edu.berkeley.cs.jqf.examples.rhino.CompilerTest", "method" : "testWithGenerator",
+{"id":"rhino", "class" : "edu.berkeley.cs.jqf.examples.rhino.CompilerTest", "method" : "testWithSplitGenerator",
 "coveragePackages" : "org/mozilla/javascript/optimizer/*:org/mozilla/javascript/CodeGenerator*"}
 ]
 
